32d4feb41ad930cbe44765d5179a066f0175a8a0
Mahmoud Sidky <ma7mod_a@live.com>
2024-04-21 15:27:45 +0300

    added user logout functionality

--- website/views.py
+++ website/views.py
@@ -13,11 +13,19 @@ def login_user(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user:
+            if user is not None:
                 login(request, user)
+                messages.success(request, 'you have been logged in successfully')
                 return redirect('home')
             else:
+                messages.error(request, 'There was an error, please try again')
                 return render(request, 'website/login.html', {'form': form})
     else:
         form = LoginForm()
     return render(request, 'website/login.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out successfully')
+    return redirect('home')
